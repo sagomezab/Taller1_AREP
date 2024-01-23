586390fded6e46dbdd1c8a4667781fbc6ad86e62
Daniel Santiago Gómez Zabala <123691122+sagomezab@users.noreply.github.com>
2024-01-23 17:27:22 -0500

    Add files via upload

---
+++ miprimera-app/src/main/java/edu/escuelaing/arem/ASE/app/URLExplorer.java
@@ -0,0 +1,18 @@
+package edu.escuelaing.arem.ASE.app;
+
+import java.net.URL;
+import java.net.MalformedURLException;
+
+public class URLExplorer {
+    public static void main(String[] args) throws MalformedURLException{
+        URL myurl = new URL("https://campusvirtual.escuelaing.edu.co:5678/moodle/ClientService.pdf?val=67&ang=6.30#grafico");
+        System.out.println("Host: " + myurl.getHost());
+        System.out.println("Authority: " + myurl.getAuthority());
+        System.out.println("Path: " + myurl.getPath());
+        System.out.println("Protocol: " + myurl.getProtocol());
+        System.out.println("Port: " + myurl.getPort());
+        System.out.println("Query: " + myurl.getQuery());
+        System.out.println("Ref: " + myurl.getRef());
+        System.out.println("File: " + myurl.getFile());
+    }
+}
